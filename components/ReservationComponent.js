05cc151b2e6324609c9e9386d3c12616925647a0
Mubbashir Ali <mubbashirali35@gmail.com>
2020-06-12 19:51:59 +0500

    Favorites (1)

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Switch, ScrollView, Button, Modal } from 'react-native';
-import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import { Picker } from '@react-native-community/picker'
 
